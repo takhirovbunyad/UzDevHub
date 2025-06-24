e8115570c9b0efb3151e3b1b8336c7430572ca98
takhirovbunyad <takhirovbunyad@gmail.com>
2025-06-24 16:50:06 +0500

    mobile version coming!

--- my_app/urls.py
+++ my_app/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from .views import first, news_view, learn_view, load_more_cards, project_list , add_project , load_more_projects
+from . import views
 
 urlpatterns = [
-    path('', first, name='first_page'),
-    path('news/', news_view, name='news'),
-    path('learn/', learn_view, name='learn_page'),
-    path('load_more_cards/', load_more_cards, name='load_more_cards'),  # agar kerak bo'lsa Dash uchun
-    path('projects/', project_list, name='project_list'),
-    path('add-project/', add_project, name='add_project') ,
-    path('load-more-projects/', load_more_projects, name='load_more_projects'),
+    path('', views.first, name='first_page'),
+    path('news/', views.news_view, name='news'),
+    path('learn/', views.learn_view, name='learn_page'),
+    path('load_more_cards/', views.load_more_cards, name='load_more_cards'),  # agar kerak bo'lsa Dash uchun
+    path('projects/', views.project_list, name='project_list'),
+    path('add-project/', views.add_project, name='add_project') ,
+    path('load-more-projects/', views.load_more_projects, name='load_more_projects'),
 ]
 
