8d7734c06c976a7713ef2dcf2213ef03f798b877
takhirovbunyad <takhirovbunyad@gmail.com>
2025-08-25 15:33:19 +0500

    testtt

--- config/settings.py
+++ config/settings.py
@@ -13,17 +13,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-3=2+j6$act&l5sz-a!g7nq$xenav!#q85+@*-mrgv0hfumo9^7'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
@@ -72,19 +67,30 @@ TEMPLATES = [
 WSGI_APPLICATION = 'config.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'uzdevhub.db',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'uzdevhub_db',
+        'USER': 'bitnet',
+        'PASSWORD': 'carZYbkuQcAqQNNikJ4fOcRXDApl5551',
+        'HOST': 'dpg-d2m3aovdiees73cgsgh0-a.oregon-postgres.render.com',
+        'PORT': '5432',
     }
 }
 
 
-# Password validation
-# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
+
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'uzdevhub.db',
+#     }
+# }
+
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -92,12 +98,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# Custom User Model
 AUTH_USER_MODEL = 'my_app.CustomUser'
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/5.2/topics/i18n/
 
 LANGUAGE_CODE = 'uz-uz'
 
@@ -110,7 +112,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
@@ -120,7 +121,6 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
-# Media files (user uploaded)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
